f1d061a3f75acfe0cd043d2f9589b37bd1726ac6
5703 - Alan Joseph <5703alj@manorama.net>
2024-01-05 16:21:46 +0530

    login page modified

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,6 @@
 "use client";
 import axios from "axios";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 export default function Login() {
@@ -13,27 +14,26 @@ export default function Login() {
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
-    console.log(userDetails);
-    if (usernameOrEmail.length == 0 || password.length == 0) {
-      setError("All field are required");
-      return null;
-    }
-    const res = await axios.post(
-      "https://lionfish-app-bihwo.ondigitalocean.app/api/auth/login",
-      userDetails
-    );
+      console.log(userDetails);
+      if (usernameOrEmail.length == 0 || password.length == 0) {
+        setError("All field are required");
+        return null;
+      }
+      const res = await axios.post(
+        "https://lionfish-app-bihwo.ondigitalocean.app/api/auth/login",
+        userDetails
+      );
 
-    if (res.status === 200) {
-      localStorage.setItem("token", `Bearer ${res.data.accessToken}`);
-      localStorage.setItem("userId", res.data.userId);
-      router.push("/");
-    } else {
+      if (res.status === 200) {
+        localStorage.setItem("token", `Bearer ${res.data.accessToken}`);
+        localStorage.setItem("userId", res.data.userId);
+        router.push("/");
+      } else {
+        setError("Invalid user credentials");
+      }
+    } catch (error) {
       setError("Invalid user credentials");
     }
-    } catch (error) {
-      setError("Invalid user credentials")
-    }
-    
   };
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -45,9 +45,13 @@ export default function Login() {
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full h-[500px] bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <div className="flex justify-center">
+              <Image src="/manorama.jpg" width={200} height={80} alt="dfdf" />
+            </div>
+{/* 
             <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
-            </h1>
+              Sign in 
+            </h1> */}
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
                 <label
@@ -83,17 +87,18 @@ export default function Login() {
                   onChange={(e) => handleChange(e)}
                 />
               </div>
-              
+            <div className="pt-5" >
+
               <button
                 type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
+                className="w-full  text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
                 Sign in
               </button>
+              </div>
               {error.length != 0 && (
                 <p className="text-center text-red-500">{error}</p>
               )}
-              
             </form>
           </div>
         </div>
